efdbce188d14f6da7fe44718a30b103b27efdc67
Linen Hongsakul <linenhongsakul@Linens-MacBook-Pro.local>
2025-12-01 18:06:23 +0700

    Update app.py and utils.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import utils import generate_questions
+from utils import generate_question
 
 with st.sidebar:
     api_key = st.text_input(label="API key",
@@ -7,36 +7,45 @@ with st.sidebar:
                             placeholder="Paste API key here...")
 
 st.title("Generate IELTS-Style Questions")
-st.text("Paste your reading passage into the text field below and click the button to generate practice qualifications and answers")
+st.text("Paste your reading passage into the text field below and click the button to generate practice questions and answers.")
 
-st.subheader("Reading Passage")
-st.text_area(label="**Reading Passage**",
+passage = st.text_area(label="**Reading Passage**",
              placeholder="Paste the full text of your reading passage here...",
              height=400)
 
-st.button(label=" ✨ Generate Questions",
+submit_button = st.button(label="✨ Generate Questions",
           type="primary")
 
 st.divider()
 
-st.header("Generated Questions & Answers")
+if submit_button:
+    st.header("Generated Questions & Answers")
+    # True / False / Not Given
+    with st.container(border=True):
+        st.subheader("True / False / Not Given")
+        data = generate_question(api_key=api_key,
+                                passage=passage,
+                                type="true_false_not_given")
+        
+        st.markdown(f"**1. {data['question']}**")
+        st.markdown(f"Answer: {data['answer']}")
+        st.markdown(data["description"])
+        
+    # Multiple choice
+    with st.container(border=True):
+        st.subheader("Multiple Choice")
+        st.markdown(f"**2. This is the second question**")
+        st.markdown(f"A) High cost of installation")
+        st.markdown(f"B) High cost of installation")
+        st.markdown(f"C) High cost of installation")
+        st.markdown(f"D) High cost of installation")
+        st.markdown(f"Answer: B")
+        st.markdown(f"This is the description.")
 
-#True /False/ Not Given
-with st.container(border=True):
-    st.subheader("True/ False/ Not given")
-    st.markdown(f"**1. This is the first question.**")
-    st.markdown(f"Answer: FALSE")
-    st.markdown(f"this is the description.")
-
-#Multiple choices 
-with st.container(border=True):
-    st.subheader("Multiple Choices")
-    st.markdown(f"**2. This is the second question.**")
-    st.markdown(f"A)")
-    st.markdown(f"B)")
-    st.markdown(f"C)")
-    st.markdown(f"D)")
-    st.markdown(f"Answer is B")
-
-#Short questions
+    # Short Answer Questions
+    with st.container(border=True):
+        st.subheader("Short Answer Questions")
+        st.markdown(f"**3. This is the third question**")
+        st.markdown(f"Answer: short answer")
+        st.markdown(f"This is the description.")
 
--- utils.py
+++ utils.py
@@ -1,20 +1,54 @@
-import google.generativeai as genai
 import json
+import google.generativeai as genai
 from typing import Literal
 
-def call_gemini_api(api_key: str,prompt:str):
+def call_gemini_api(api_key: str, prompt: str):
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel("gemini-2.5-flash-lithe")
-    data = model.generate_content (prompt,
-                                   generative_config={
-                                       "response_mime_type": "application/json"
-                                   })
-    data = json.loads(response.text)
-                                    
+    model = genai.GenerativeModel("gemini-2.5-flash-lite")
+    response = model.generate_content(prompt,
+                                      generation_config={
+                                          "response_mime_type": "application/json"
+                                      })
 
-def generate_question(api_key:str,
-                      passage: str,
+    data = json.loads(response.text)
+    return data
+
+def generate_question(api_key: str, 
+                      passage: str, 
                       type: Literal["true_false_not_given",
-                                    "multiple_choices",
+                                    "multiple_choice",
                                     "short_answer"]):
-    pass
+    
+    if type == "true_false_not_given":
+        prompt = f"""Generate an IELTS-style True/False/Not Given question, based on the passage below.
+
+        return the output in **strict JSON** format with the following structure.
+        **Expected Output Schema**
+        | Column | Type | Description |
+        |---------|------|--------------|
+        | question | str | IELTS-style question based on the passage |
+        | answer | str | TRUE / FALSE / NOT GIVEN |
+        | description | str | Description of answer |
+
+        Example:
+        {{
+            "question": "Dinosaurs are the biggest animals to ever live on Earth.",
+            "answer": FALSE,
+            "description": Although dinosaurs were among the largest land animals to ever live on Earth, they were not the biggest animals. The blue whale, which is alive today, is actually larger than any known dinosaur.,
+        }}
+
+        Passage:
+        {passage}
+        """
+        return call_gemini_api(api_key, prompt)
+
+if __name__ == '__main__':
+    api_key = "AIzaSyD_9P-QWjjJKd23chwPSxpbmC72Dhiyx5Q"
+    print(generate_question(api_key=api_key,
+                            passage=""""Marie Curie is probably the most famous woman scientist who has ever lived. Born
+Maria Sklodowska in Poland in 1867, she is famous for her work on radioactivity, and
+was twice a winner of the Nobel Prize. With her husband, Pierre Curie, and Henri
+Becquerel, she was awarded the 1903 Nobel Prize for Physics, and was then sole winner
+of the 1911 Nobel Prize for Chemistry. She was the first woman to win a Nobel Prize.
+                            """,
+                            type="true_false_not_given"))
